60e72f153d0731f9b60cf050a7bfffd26377c6a5
Phil <natsudragoneel282003@gmail.com>
2026-07-31 09:51:58 -0700

    Foundational routing was made for navigational purposes on the web page.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route, useParams } from 'react-router'
 import './index.css'
+
 import App from './App.tsx'
+import Navbar from './components/Navbar.tsx'
+import Card from './components/Card.tsx'
+import SelectedTable from './pages/SelectedTable.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+        <Route path ="/" element={<Navbar/>}>
+          <Route index={true} element={<App/>} />
+           <Route path="/table/:id" element={<SelectedTable/>} /> 
+        </Route>
+    </Routes>
+  </BrowserRouter>
 )
