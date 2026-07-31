5991cb76f359ed2f47d408ba79ce0355a43c306c
Phil <natsudragoneel282003@gmail.com>
2026-07-31 09:43:57 -0700

    Initialized two pages where user can create a post, and users can click on a Post to look at its details

---
+++ src/pages/SelectedTable.tsx
@@ -0,0 +1,12 @@
+// This TSX files renders a single post that displays the post, post content, upvotes, and comments.
+
+import { useState } from 'react'
+
+const SelectedTable = () => {
+    return(
+        <>
+        </>
+    )
+}
+
+export default SelectedTable
