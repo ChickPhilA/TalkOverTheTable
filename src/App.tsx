37e70cbd7646d72b6f598d49fc1d2b0c886d94ce
Phil <natsudragoneel282003@gmail.com>
2026-07-31 09:51:04 -0700

    Created functionality for posts being retrieved and read from the database on the Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,120 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+// This TSX file will also act as the "Home" page.
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useState, useEffect } from 'react'
+import { supabase } from './client'
+
+import './App.css'
+import Card from './components/Card'
+
+import type { Table } from './types/types'
+
+const App = () => {
+
+  const [tables, setTables] = useState<Table[]>([]) // our tables will be stored here
+  
+
+  // We will need an API call to see if posts exist from the database.
+  useEffect( () => {
+    const fetchTables = async () => {
+      const { data } = await supabase
+      .from('tables')
+      .select()
+      .order('created_at', {ascending: true})
+
+      const posts = data as Table[] // infers that destructuring the posts variable will be a Table[] type.
+
+      setTables(posts)
+    }
+
+    fetchTables() // makes the API call!!
+  }, [])
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
+      <div className="flex flex-row max-w-full">
+        {/* The left item is the list of posts. But for now, we'll keep it full. */}
         <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
+          {tables && tables.length > 0
+            ?
+            tables.map(table => 
+              <Card key={table.id} post={table} />
+            ) 
+            :
+            <h2 className="brown mx-auto">
+              There are no tables to discuss over! Would you like to start a new one?
+            </h2>
+          }
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      </div>
     </>
   )
 }
