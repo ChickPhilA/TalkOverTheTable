cac44082237e3d778b334acb4c58f8df819dfcb5
Phil <natsudragoneel282003@gmail.com>
2026-08-01 15:42:58 -0700

    Added a New Table button feature, which leads to the CreateTable.tsx form.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,13 @@ import { Outlet, NavLink } from 'react-router'
 function Navbar() {
     return(
         <div>
-            <nav className="bg-brown min-h-16 flex items-center">
+            <nav className="bg-brown min-h-16 flex justify-around  items-center">
                 <NavLink to="/" end className="font-brand font-bold text-3xl text-mustard">
                     TalkOverTheTable
+                </NavLink>
+
+                <NavLink to="create" className="bg-mustard hover:bg-mustard-deep transition-colors duration-300 text-ink font-bold px-4 py-2 rounded-lg">
+                    Create Table +
                 </NavLink>
             </nav>
             <Outlet/>
