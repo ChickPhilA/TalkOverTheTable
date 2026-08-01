76d2c838d63279ed9e20455df3d92d92003a3900
Phil <natsudragoneel282003@gmail.com>
2026-08-01 15:40:25 -0700

    Made a Not Found page in case URL routing does not exist.

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router'
 const NotFound = () => {
     return (
     <>
-       <div className="mx-auto text-center brown">
-            <h1>
-            Whoops! Looks like no tables to yap over were not found here...
-                <br></br>
-            Click <Link className="text-white" to="/"> to </Link> return back to the Home page. Let's search for a table that exists!
+       <div className="mx-auto mt-40 text-center brown font-heading">
+            <h1 className="text-4xl">
+            Whoops! Looks like no tables were not found over here...
+                <br></br><br></br>
+            Click <Link className="text-mustard hover:text-mustard/60 transition-colors duration-200 cursor-pointer" to="/"> here </Link> to return back to the Home page. Let's search for a table that exists!
             </h1>
         </div>
     </>
