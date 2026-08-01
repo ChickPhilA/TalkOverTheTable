2c662b9fd16bdc9cbdfdc21568344de02978332c
Phil <natsudragoneel282003@gmail.com>
2026-08-01 13:30:32 -0700

    Created a NewTable type for table creation upon a Create operation to Supabase.

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,8 +3,17 @@ export interface Table {
     created_at: string
     title: string
     upvotes: number
-    context: string
+    content?: string
     image_url?: string
     comments: string[]
-    category: string
+    category?: string
 }
+
+/* Below is the type for our CreateTable.tsx form. We import NewTable for creating a new table (post).
+ If we use the Table type instead of NewTable type to create a new table form, then
+ TypeScript will give errors for not having values that are created upon creating to Supabase
+provided already (e.g. 'id', 'created_at', etc.)
+ */
+
+export type NewTable = Omit<Table, 'id' | 'created_at' | 'upvotes' | 'comments'>
+
